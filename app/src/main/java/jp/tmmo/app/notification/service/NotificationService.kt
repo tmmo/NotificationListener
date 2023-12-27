1a9c0e6ea996a98e13f25c2eb6d793556dd999ad
tmmo <tmmo4792@gmail.com>
2023-12-27 23:16:40 +0900

    Study notificationlistenerservice and notification

--- app/src/main/java/jp/tmmo/app/notification/service/NotificationService.kt
+++ app/src/main/java/jp/tmmo/app/notification/service/NotificationService.kt
@@ -1,7 +1,55 @@
 package jp.tmmo.app.notification.service
 
 import android.service.notification.NotificationListenerService
+import android.service.notification.StatusBarNotification
+import android.util.Log
 
 class NotificationService : NotificationListenerService() {
-    override fun
+    companion object {
+        private const val TAG = "NotificationService"
+        private val apps = listOf(
+            "jp.co.sonymusic.communication.sakurazaka",
+            "jp.co.sonymusic.communication.nogizaka"
+        )
+    }
+
+    override fun onListenerConnected() {
+        super.onListenerConnected()
+        Log.d(TAG, "onListenerConnected")
+        val notifications = activeNotifications
+        Log.d(TAG, "notifications=$notifications")
+        notifications.forEach {
+            notify(it)
+        }
+    }
+
+    private fun notify(statusBarNotification: StatusBarNotification) {
+        if (!apps.contains(statusBarNotification.packageName)) {
+            return
+        }
+        val notification = statusBarNotification.notification
+        Log.d(TAG, "notification=$notification")
+        Log.d(TAG, "ticketText=${notification.tickerText}")
+        val extras = notification.extras
+        extras.keySet().forEach {
+            Log.d(TAG, "key=$it value=${extras.get(it)}")
+        }
+        notification.actions?.forEach {
+            Log.d(TAG, "title=${it.title} action=$it")
+        }
+        val intent = notification.contentIntent
+        Log.d(TAG, "contentIntent=$intent")
+        intent.send()
+    }
+
+    override fun onNotificationPosted(sbn: StatusBarNotification?) {
+        super.onNotificationPosted(sbn)
+        sbn ?: return
+        notify(sbn)
+    }
+
+    override fun onListenerDisconnected() {
+        super.onListenerDisconnected()
+        Log.d(TAG, "onListenerDisconnected")
+    }
 }
